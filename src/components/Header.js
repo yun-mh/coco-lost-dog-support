8893f6be3e4915e16e5ba9f41cdcf531211cf931
Minho Yun <minhoyun.dev@gmail.com>
2020-11-10 18:37:07 +0900

    Refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,103 +1,200 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { Calendar, Info, PenTool, X } from "react-feather";
+import tw from "twin.macro";
 import moment from "moment";
 import Loader from "./Loader";
+
+const NavContainer = styled.nav`
+  ${tw`fixed w-full mx-auto bg-white`}
+`;
+
+const NavBar = styled.div`
+  ${tw`max-w-6xl container  px-2 sm:px-6 lg:px-8`}
+`;
+
+const NavBarContainer = styled.div`
+  ${tw`relative flex items-center justify-between h-16`}
+`;
+
+const MenuBtnContainer = styled.div`
+  ${tw`absolute inset-y-0 left-0 flex items-center sm:hidden`}
+`;
+
+const Title = styled.div`
+  ${tw`flex-1 flex items-center justify-center sm:items-stretch sm:justify-start`}
+`;
+
+const TitleContainer = styled.div`
+  ${tw`flex flex-shrink-0 items-end`}
+`;
+
+const TitleText = styled.span`
+  ${tw`ml-3 hidden sm:block font-bold text-xl text-gray-600 antialiased`}
+`;
+
+const MobileMenu = styled.div`
+  ${tw`sm:hidden`}
+`;
+
+const MenuList = styled.div``;
+
+const MenuItemContainer = styled.div`
+  ${tw`w-full flex items-center px-2 pt-2 pb-3`}
+`;
+
+const ItemRow = styled.div`
+  ${tw`my-1`}
+`;
+
+const FlexContainer = styled.div`
+  ${tw`flex items-center`}
+`;
+
+const FlexColContainer = styled.div`
+  ${tw`flex flex-col`}
+`;
+
+const Image = styled.img`
+  ${tw`object-cover rounded-lg`}
+`;
+
+const Name = styled.span`
+  ${tw`font-semibold`}
+`;
+
+const GenderSymbol = styled.span`
+  ${tw`text-base`}
+`;
+
+const Gender = styled.span`
+  ${tw`ml-2 text-sm`}
+`;
 
 const Header = ({ user, loading, data }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => {
-      setIsOpen(!isOpen);
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <nav className="fixed w-full mx-auto bg-white">
-        <div className="max-w-7xl container  px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    { loading && data === undefined && (
-                      <Loader size={36} />  
-                    )}
-                    { !loading && data && (
-                        <button onClick={toggleOpen} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
-                            { !isOpen ? <Info /> : <X />}
-                        </button>
-                    ) }
-                </div>
-            
-                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                    <div className="flex flex-shrink-0 items-end">
-                        <img className="block h-8 w-auto" src={require("../assets/images/logo.png").default} alt="logo" />
-                        <span className="ml-3 hidden sm:block font-bold text-xl text-gray-600 antialiased">COCO迷子サポート</span>
-                    </div>
-                </div>
-            
-                { user && (
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <div className="flex hidden md:block">
-                            <a href="#" className="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
-                                記事作成
-                            </a>
-                        </div>
-                        <div className="flex md:hidden">
-                            <a href="#" className="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
-                                <PenTool />
-                            </a>
-                        </div>
-                    </div>
-                )}
-            </div>
-        </div>
-
-        <div className={ isOpen ? "block sm:hidden" : "hidden sm:hidden"}>
-            { data && data.viewDog && (
-                <>
-                    <div className="w-full flex items-center px-2 pt-2 pb-3">
-                        <img className="w-32 h-32 sm:w-48 sm:h-48 object-cover rounded-lg" src={data.viewDog.image} alt="dog" />
-                        <div className="ml-3 w-full flex flex-col justify-between">
-                            <div className="my-1">
-                                <div className="flex items-center">
-                                    <span className="text-xl font-semibold mr-3">{ data.viewDog.name }</span>
-                                    {data.viewDog.gender === "male" ? (
-                                        <>(<span className="text-blue-500 text-base">&#9794;</span><span className="ml-2 text-sm">男)</span></>
-                                    ) : (
-                                        <>(<span className="text-red-400 text-base">&#9792;</span><span className="ml-2 text-sm">女)</span></>
-                                    )}
-                                </div>
-                            </div>
-                            <div className="my-1">
-                                <span>{data.viewDog.breed}</span>
-                            </div>
-                            <div className="my-1">
-                                <div className="flex items-center text-sm">
-                                    <Calendar size={14} className="mr-1" />
-                                    {moment(data.viewDog.birthdate).format("ll")}
-                                    ({moment().diff(data.viewDog.birthdate, "years")}歳)
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full flex items-center px-2 pt-2 pb-3 shadow">
-                        <div className="w-full my-1 border rounded text-sm p-2 relative">
-                            <div className="absolute bg-primary text-white px-1 rounded">飼い主</div>
-                            <div className="flex items-center justify-end">
-                                <div className="flex flex-col items-end mr-3">
-                                    <span className="font-semibold">
-                                        {data.viewDog.user.username}
-                                    </span>
-                                    <span>
-                                    {data.viewDog.user.email}
-                                    </span>
-                                </div>
-                                <img className="w-12 h-12 object-cover rounded-lg" src={data.viewDog.user.avatar} alt="user" />
-                            </div>
-                        </div>    
-                    </div>
-                </>
+    <NavContainer>
+      <NavBar>
+        <NavBarContainer>
+          <MenuBtnContainer>
+            {loading && data === undefined && <Loader size={36} />}
+            {!loading && data && (
+              <button
+                onClick={toggleOpen}
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+              >
+                {!isOpen ? <Info /> : <X />}
+              </button>
             )}
-        </div>
-    </nav>
-  )
+          </MenuBtnContainer>
+
+          <Title>
+            <TitleContainer>
+              <img
+                className="block h-8 w-auto"
+                src={require("../assets/images/logo.png").default}
+                alt="logo"
+              />
+              <TitleText>COCO迷子サポート</TitleText>
+            </TitleContainer>
+          </Title>
+
+          {user && (
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="flex hidden md:block">
+                <a
+                  href="#"
+                  className="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                >
+                  記事作成
+                </a>
+              </div>
+              <div className="flex md:hidden">
+                <a
+                  href="#"
+                  className="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                >
+                  <PenTool />
+                </a>
+              </div>
+            </div>
+          )}
+        </NavBarContainer>
+      </NavBar>
+
+      <MobileMenu className={isOpen ? "block" : "hidden"}>
+        {data && data.viewDog && (
+          <MenuList>
+            <MenuItemContainer>
+              <Image
+                className="w-32 h-32 sm:w-48 sm:h-48"
+                src={data.viewDog.image}
+                alt="dog"
+              />
+              <FlexColContainer className="ml-3 w-full justify-between">
+                <ItemRow>
+                  <FlexContainer>
+                    <Name className="text-xl mr-3">{data.viewDog.name}</Name>
+                    {data.viewDog.gender === "male" ? (
+                      <>
+                        (
+                        <GenderSymbol className="text-blue-500">
+                          &#9794;
+                        </GenderSymbol>
+                        <Gender>男)</Gender>
+                      </>
+                    ) : (
+                      <>
+                        (
+                        <GenderSymbol className="text-red-400">
+                          &#9792;
+                        </GenderSymbol>
+                        <Gender>女)</Gender>
+                      </>
+                    )}
+                  </FlexContainer>
+                </ItemRow>
+                <ItemRow>
+                  <span>{data.viewDog.breed}</span>
+                </ItemRow>
+                <ItemRow>
+                  <FlexContainer className="text-sm">
+                    <Calendar size={14} className="mr-1" />
+                    {moment(data.viewDog.birthdate).format("ll")}(
+                    {moment().diff(data.viewDog.birthdate, "years")}歳)
+                  </FlexContainer>
+                </ItemRow>
+              </FlexColContainer>
+            </MenuItemContainer>
+            <MenuItemContainer className="shadow">
+              <div className="w-full my-1 border rounded text-sm p-2 relative">
+                <div className="absolute bg-primary text-white px-1 rounded">
+                  飼い主
+                </div>
+                <FlexContainer className="justify-end">
+                  <FlexColContainer className="items-end mr-3">
+                    <Name>{data.viewDog.user.username}</Name>
+                    <span>{data.viewDog.user.email}</span>
+                  </FlexColContainer>
+                  <Image
+                    className="w-12 h-12"
+                    src={data.viewDog.user.avatar}
+                    alt="user"
+                  />
+                </FlexContainer>
+              </div>
+            </MenuItemContainer>
+          </MenuList>
+        )}
+      </MobileMenu>
+    </NavContainer>
+  );
 };
 
 export default Header;
--- src/routes/Dog/DogPresenter.js
+++ src/routes/Dog/DogPresenter.js
@@ -6,88 +6,198 @@ import Loader from "../../components/Loader";
 import { useHistory } from "react-router-dom";
 
 const DogPresenter = ({ user, loading, data }) => {
-    const history = useHistory();
+  const history = useHistory();
 
-    if (!loading && data === undefined) {
-        history.push("/");
-    }
+  if (!loading && data === undefined) {
+    history.push("/");
+  }
 
-    console.log(data)
+  console.log(data);
 
-    return (
-        <>
-            <Header user={user} loading={loading} data={data} />
-            <div className="pt-16">
-                { loading ? (
-                    <div className="w-full h-32 flex items-center justify-center">
-                        <Loader />
+  return (
+    <>
+      <Header user={user} loading={loading} data={data} />
+      <div className="pt-16">
+        {loading ? (
+          <div className="w-full h-32 flex items-center justify-center">
+            <Loader />
+          </div>
+        ) : data !== undefined ? (
+          <>
+            <div className="hidden sm:block m-3 bg-white rounded shadow">
+              <div className="w-full flex items-center px-2 pt-2 pb-3">
+                <div className="w-1/2 lg:w-2/3 flex items-center px-2 pt-2 pb-3">
+                  <img
+                    className="w-32 h-32 object-cover rounded-lg"
+                    src={data.viewDog.image}
+                    alt="dog"
+                  />
+                  <div className="ml-3 w-full flex flex-col justify-between">
+                    <div className="my-1">
+                      <div className="flex items-center">
+                        <span className="text-xl font-semibold mr-3">
+                          {data.viewDog.name}
+                        </span>
+                        {data.viewDog.gender === "male" ? (
+                          <>
+                            (
+                            <span className="text-blue-500 text-base">
+                              &#9794;
+                            </span>
+                            <span className="ml-2 text-sm">男)</span>
+                          </>
+                        ) : (
+                          <>
+                            (
+                            <span className="text-red-400 text-base">
+                              &#9792;
+                            </span>
+                            <span className="ml-2 text-sm">女)</span>
+                          </>
+                        )}
+                      </div>
                     </div>
-                ) : data !== undefined ? (
-                    <>
-                        <div className="hidden sm:block m-3 bg-white rounded shadow">
-                            <div className="w-full flex items-center px-2 pt-2 pb-3">
-                                <div className="w-1/2 lg:w-2/3 flex items-center px-2 pt-2 pb-3">
-                                    <img className="w-32 h-32 object-cover rounded-lg" src={data.viewDog.image} alt="dog" />
-                                    <div className="ml-3 w-full flex flex-col justify-between">
-                                        <div className="my-1">
-                                            <div className="flex items-center">
-                                                <span className="text-xl font-semibold mr-3">{ data.viewDog.name }</span>
-                                                {data.viewDog.gender === "male" ? (
-                                                    <>(<span className="text-blue-500 text-base">&#9794;</span><span className="ml-2 text-sm">男)</span></>
-                                                ) : (
-                                                    <>(<span className="text-red-400 text-base">&#9792;</span><span className="ml-2 text-sm">女)</span></>
-                                                )}
-                                            </div>
-                                        </div>
-                                        <div className="my-1">
-                                            <span>{data.viewDog.breed}</span>
-                                        </div>
-                                        <div className="my-1">
-                                            <div className="flex items-center text-sm">
-                                                <Calendar size={14} className="mr-1" />
-                                                {moment(data.viewDog.birthdate).format("ll")}
-                                                ({moment().diff(data.viewDog.birthdate, "years")}歳)
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="w-1/2 lg:w-1/3 flex items-center px-2 pt-2 pb-3">
-                                    <div className="w-full flex flex-col justify-between h-32 my-1 border rounded text-sm p-3">
-                                        <div className="w-12 text-center bg-primary text-white px-1 rounded">飼い主</div>
-                                        <div className="flex items-center justify-end">
-                                            <div className="flex flex-col items-end mr-3">
-                                                <span className="font-semibold">
-                                                    {data.viewDog.user.username}
-                                                </span>
-                                                <span>
-                                                {data.viewDog.user.email}
-                                                </span>
-                                            </div>
-                                            <img className="w-12 h-12 object-cover rounded-lg" src={data.viewDog.user.avatar} alt="user" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="m-3 bg-white rounded shadow min-h-half p-5">
-                            <h2 className="text-2xl font-semibold mb-3 text-gray-700">迷子状況</h2>
-                            { !data.viewDog.isMissed ? (
-                                <div className="h-64 flex items-center justify-center">
-                                    <Smile size={40} className="text-primary mr-3" />
-                                    <span className="text-xl text-gray-500">迷子状態ではありません。</span>
-                                </div>
-                            ) : (
-                                <div>迷子！</div>
-                            )}
-                        </div>
-                    </>
-                ) : (
-                    <div>None</div>
-                )}
+                    <div className="my-1">
+                      <span>{data.viewDog.breed}</span>
+                    </div>
+                    <div className="my-1">
+                      <div className="flex items-center text-sm">
+                        <Calendar size={14} className="mr-1" />
+                        {moment(data.viewDog.birthdate).format("ll")}(
+                        {moment().diff(data.viewDog.birthdate, "years")}歳)
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-1/2 lg:w-1/3 flex items-center px-2 pt-2 pb-3">
+                  <div className="w-full flex flex-col justify-between h-32 my-1 border rounded text-sm p-3">
+                    <div className="w-12 text-center bg-primary text-white px-1 rounded">
+                      飼い主
+                    </div>
+                    <div className="flex items-center justify-end">
+                      <div className="flex flex-col items-end mr-3">
+                        <span className="font-semibold">
+                          {data.viewDog.user.username}
+                        </span>
+                        <span>{data.viewDog.user.email}</span>
+                      </div>
+                      <img
+                        className="w-12 h-12 object-cover rounded-lg"
+                        src={data.viewDog.user.avatar}
+                        alt="user"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </>
-    );
-}
+
+            <div className="m-3 bg-white rounded shadow min-h-half p-5">
+              <h2 className="text-2xl font-semibold mb-3 text-gray-700">
+                迷子状況
+              </h2>
+              {!data.viewDog.isMissed ? (
+                <div className="h-64 flex items-center justify-center">
+                  <Smile size={40} className="text-primary mr-3" />
+                  <span className="text-xl text-gray-500">
+                    迷子状態ではありません。
+                  </span>
+                </div>
+              ) : (
+                <div>
+                  <div class="w-full my-4">
+                    <div class="rounded-sm">
+                      <div
+                        class="border border-b-0 bg-gray-100 px-10 py-6"
+                        id="headingOne"
+                      >
+                        <button
+                          class="underline text-blue-500 hover:text-blue-700 focus:outline-none"
+                          type="button"
+                        >
+                          Collapsible Group Item #1
+                        </button>
+                      </div>
+                      <div x-show="show" class="border border-b-0 px-10 py-6">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high
+                        life accusamus terry richardson ad squid. 3 wolf moon
+                        officia aute, non cupidatat skateboard dolor brunch.
+                        Food truck quinoa nesciunt laborum eiusmod. Brunch 3
+                        wolf moon tempor, sunt aliqua put a bird on it squid
+                        single-origin coffee nulla assumenda shoreditch et.
+                        Nihil anim keffiyeh helvetica, craft beer labore wes
+                        anderson cred nesciunt sapiente ea proident. Ad vegan
+                        excepteur butcher vice lomo. Leggings occaecat craft
+                        beer farm-to-table, raw denim aesthetic synth nesciunt
+                        you probably haven't heard of them accusamus labore
+                        sustainable VHS.
+                      </div>
+                    </div>
+                    <div class="rounded-sm">
+                      <div
+                        class="border border-b-0 bg-gray-100 px-10 py-6"
+                        id="headingOne"
+                      >
+                        <button
+                          class="underline text-blue-500 hover:text-blue-700 focus:outline-none"
+                          type="button"
+                        >
+                          Collapsible Group Item #2
+                        </button>
+                      </div>
+                      <div x-show="show" class="px-10 py-6">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high
+                        life accusamus terry richardson ad squid. 3 wolf moon
+                        officia aute, non cupidatat skateboard dolor brunch.
+                        Food truck quinoa nesciunt laborum eiusmod. Brunch 3
+                        wolf moon tempor, sunt aliqua put a bird on it squid
+                        single-origin coffee nulla assumenda shoreditch et.
+                        Nihil anim keffiyeh helvetica, craft beer labore wes
+                        anderson cred nesciunt sapiente ea proident. Ad vegan
+                        excepteur butcher vice lomo. Leggings occaecat craft
+                        beer farm-to-table, raw denim aesthetic synth nesciunt
+                        you probably haven't heard of them accusamus labore
+                        sustainable VHS.
+                      </div>
+                    </div>
+                    <div class="rounded-sm">
+                      <div
+                        class="border bg-gray-100 px-10 py-6"
+                        id="headingOne"
+                      >
+                        <button
+                          class="underline text-blue-500 hover:text-blue-700 focus:outline-none"
+                          type="button"
+                        >
+                          Collapsible Group Item #3
+                        </button>
+                      </div>
+                      <div x-show="show" class="border px-10 py-6">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high
+                        life accusamus terry richardson ad squid. 3 wolf moon
+                        officia aute, non cupidatat skateboard dolor brunch.
+                        Food truck quinoa nesciunt laborum eiusmod. Brunch 3
+                        wolf moon tempor, sunt aliqua put a bird on it squid
+                        single-origin coffee nulla assumenda shoreditch et.
+                        Nihil anim keffiyeh helvetica, craft beer labore wes
+                        anderson cred nesciunt sapiente ea proident. Ad vegan
+                        excepteur butcher vice lomo. Leggings occaecat craft
+                        beer farm-to-table, raw denim aesthetic synth nesciunt
+                        you probably haven't heard of them accusamus labore
+                        sustainable VHS.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
+          <div>None</div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default DogPresenter;
